444270e6b2b59edd873c51fa6c2eec3d5b2ba029
vinaybl7 <vinaybl@gmail.com>
2013-07-31 21:45:36 -0700

    first commit

---
+++ cbaby1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cbaby1::Application.config.secret_key_base = 'ba0a44679eafaef743da603ede0f2aac19eb85313bc635aecbc7fcedc9bc40b5081d0e2c565376f395cb2b5cd3f089dc8557c51c63fd8c0ed822743559c4d29c'
---
+++ cbaby1/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cbaby1::Application.config.session_store :cookie_store, key: '_cbaby1_session'
